537fdb32edd4bca15f130265f8ea0a450866e1f7
Paalamugan <42642576+paalamugan@users.noreply.github.com>
2024-08-24 21:37:20 +0530

    chore: Update commitlint.config.ts

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,22 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'always',
+      [
+        'lower-case',
+        'upper-case',
+        'camel-case',
+        'kebab-case',
+        'pascal-case',
+        'sentence-case',
+        'snake-case',
+        'start-case',
+      ],
+    ],
+  },
 };
 
 export default Configuration;
